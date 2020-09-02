54cc80e96c40e080b7f9ef10948dc01acb586abd
Matt Gozel <gozel002@umn.edu>
2020-09-02 10:14:55 -0500

    Added ViewModel

---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,25 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Humble";
+            CurrentPlayer.Class = "Fighter";
+            CurrentPlayer.HitPoints = 100;
+            CurrentPlayer.ExperiencePoints = 0;
+            CurrentPlayer.Level = 1;
+            CurrentPlayer.Gold = 1000000;
+        }
+    }
+}
