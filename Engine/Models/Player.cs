42245c13d38a3256e4c9b1a228ee2e4c6ed77228
Matt Gozel <gozel002@umn.edu>
2020-09-02 20:28:22 -0500

    Added ability to change player data based on in game events

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -1,18 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    public class Player
+    public class Player : INotifyPropertyChanged
     {
-        public string Name { get; set; }
-        public string Class { get; set; }
-        public int HitPoints { get; set; }
-        public int ExperiencePoints { get; set; }
-        public int Level { get; set; }
-        public int Gold { get; set; }
+        private string _name;
+        private string _class;
+        private int _hitPoints;
+        private int _experiencePoints;
+        private int _level;
+        private int _gold;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Class
+        {
+            get { return _class; }
+            set
+            {
+                _class = value;
+                OnPropertyChanged("Class");
+            }
+        }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            set
+            {
+                _hitPoints = value;
+                OnPropertyChanged("HitPoints");
+            }
+        }
+        public int ExperiencePoints
+        {
+            get { return _experiencePoints; }
+            set
+            {
+                _experiencePoints = value;
+                OnPropertyChanged("ExperiencePoints");
+            }
+        }
+        public int Level
+        {
+            get { return _level; }
+            set
+            {
+                _level = value;
+                OnPropertyChanged("Value");
+            }
+        }
+        public int Gold
+        {
+            get { return _gold; }
+            set
+            {
+                _gold = value;
+                OnPropertyChanged("Gold");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
